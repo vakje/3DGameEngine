b6183b517cd7946861e9a0c32a7d5f98e830ee42
vakje <man263170@gmail.com>
2025-11-15 02:50:53 +0400

    testing new obj loader for rendering detailed objects in engine

--- _Project_GLFW_/Main.cpp
+++ _Project_GLFW_/Main.cpp
@@ -6,10 +6,100 @@ const int WIDTH = 2560;
 const int HEIGHT = 1440;
 const char* TITLE = "3D GAME ENGINE";
 
-Game game;
+std::vector<float> Vertices;
+std::vector<unsigned int> Indices;
+std::vector<float>UVcoordinates;
+
+void TexturedObjectFileParser(const std::string& filename, std::vector<float>& Vertices, std::vector<unsigned int>& Indices,
+	std::vector<float>&UVcoordinates)
+{
+	//this line makes full path of the file must be like ..root\\user\\tools\\file.obj
+	std::filesystem::path Toolspath = std::filesystem::current_path() / "TOOLS" / filename;
+	// this must take and transform that in string
+	std::string PATH = Toolspath.string();
+	//this must split the string and take last file
+	std::string lastfile = UTils::SplitPath(PATH);
+	std::cout << "Meshe's Name:  " << lastfile << "  " << std::endl;
+	if (lastfile.find(".obj") == std::string::npos)
+	{
+		std::cout << "this is not a obj file" << std::endl;
+	}
+	std::ifstream file(PATH);
+
+	if (!file.is_open())
+	{
+		std::cout << "file not found" << std::endl;
+	}
+
+	std::string line;
+	std::string v;
+	std::string i;
+	std::vector<std::string> StringV;
+	std::vector<std::string> StringI;
+	std::vector<std::string> StringIV;
+	std::vector<std::string> StringUV;
+	std::getline(file, line);
+
+
+	while (std::getline(file, line))
+	{
+		
+		if (line[0] == 'v' and line[1] != 't')
+		{
+			StringV = UTils::SplitString(line, " ");
+		
+			for (size_t i = 1; i<StringV.size(); i++)
+			{				
+				Vertices.push_back(std::stof(StringV[i]));						
+			}			
+		}
+		else if (line.substr(0, 2) == "vt")
+		{				
+			StringUV = UTils::SplitString(line, " ");
+
+		    for (size_t i=1; i<StringUV.size(); i++)
+		    {										 
+				UVcoordinates.push_back(std::stof(StringUV[i]));									
+		    }			
+		}	
+		else if (line[0] == 'f')
+		{
+
+			StringI = UTils::SplitString(line, " ");
+
+			
+			for (size_t i =1; i<StringI.size();i++)
+			{
+				    // 3/1,2/1,3/2 
+				
+					if (StringI[i].size() == 1)
+					{					
+						//part is index now
+						Indices.push_back(std::stoul(StringI[i]) - 1);
+						std::cout << "parsed obj file that contains only indices\n";
+
+					}
+					else if (StringI[i].size() >= 3)
+					{
+						//now part is multiple indices
+						StringIV = UTils::SplitString(StringI[i],"/");
+
+						for (auto& indeces : StringIV) 
+						{
+							Indices.push_back(std::stoul(indeces) - 1);						   							
+						}
+					}			
+			}
+		}	
+	}
+	file.close();
+}
+
 int main()
 {
 	try {
+		
+		Game game;
 		Window win(WIDTH, HEIGHT, TITLE);
 		int frames = 0;
 
@@ -50,5 +140,21 @@ int main()
 	}
 
 	std::cout << "-----------------------------------------------\n" << std::flush;
+
+
+	TexturedObjectFileParser("cubeTextured.obj", Vertices,Indices,UVcoordinates);
+
+	std::cout << "\n-----------------UVCoords----------------------\n";
+	std::cout << "UVcoordinates size: " << UVcoordinates.size() << "\n";
+	for (auto& item : UVcoordinates) {
+		std::cout << std::fixed << std::setprecision(6) << item << "\n";
+	}
+	std::cout << "\n-----------------Vertices----------------------\n";
+	std::cout << "vertices size: " << Vertices.size() << "\n";
+	std::for_each(Vertices.begin(), Vertices.end(),[](float x){std::cout<<x<<" "; });
+	std::cout << "\n-----------------Indices----------------------\n";
+	std::for_each(Indices.begin(), Indices.end(), [](float x) {std::cout << x << " "; });
+	
+
      
 }
